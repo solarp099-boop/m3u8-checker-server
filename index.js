eb6c8480131291e94d5e4eade8d27a50dcfd6c0a
solarp099 <solarp099@gmail.com>
2026-05-04 23:27:48 -0500

    checker PRO final

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ let collection;
 
 // --- CONFIGURACIÓN DE LOGOS ---
 // CAMBIA ESTO: Pon tu usuario y repositorio real de GitHub
-const GITHUB_LOGOS_BASE = "https://github.com/solarp099-boop/logos-tv.git";
+const GITHUB_LOGOS_BASE = "https://github.com/solarp099-boop/logos-tv";
 
 // Función para generar la URL del logo automáticamente
 function generateLogoUrl(name) {
